cc3e8a55e3e77edf20b3f2aba89d847d360c2403
gustavouhlein <gustavogilli08@gmail.com>
2024-01-13 21:02:53 -0300

    Criação da estrutura de componentes da página inicial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { CardGrandeComponent } from './components/card-grande/card-grande.component';
+import { CardPequenoComponent } from './components/card-pequeno/card-pequeno.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TituloComponent,
+    CardGrandeComponent,
+    CardPequenoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
